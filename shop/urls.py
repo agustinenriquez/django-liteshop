04605136a20eea04c0d5c50edfc3b8e0dfa9b988
m0tz <agustin_enriquez@hotmail.com>
2020-06-17 23:06:18 -0300

    item detail view

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from shop.views import ItemList
+from shop.views import ItemList, ItemDetailView
 
 
 urlpatterns = [
         path("", ItemList.as_view(), name="main-shop"),
+        path("/item/<int:pk>/", ItemDetailView.as_view(), name="item-detail"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,11 @@
 from .models import Item
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 # Create your views here.
 class ItemList(ListView):
+    model = Item
+
+
+class ItemDetailView(DetailView):
     model = Item
