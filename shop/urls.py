c872ed90f0df59f03cada4cc9fba358dc6b19955
m0tz <agustin_enriquez@hotmail.com>
2020-06-19 22:12:41 -0300

    cartitem form html

--- shop/urls.py
+++ shop/urls.py
@@ -4,6 +4,6 @@ from shop.views import ItemList, ItemDetailView, CartItemCreateView
 
 urlpatterns = [
         path("", ItemList.as_view(), name="main-shop"),
-        path("/item/<int:pk>/", ItemDetailView.as_view(), name="item-detail"),
+        path("item/<int:pk>/", ItemDetailView.as_view(), name="item-detail"),
         path("cartitem/add/<int:pk>", CartItemCreateView.as_view(), name="cartitem-create"),
 ]
