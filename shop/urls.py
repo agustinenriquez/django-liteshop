481c075c38db53b643001aa82d4c9c2ecd9896c4
m0tz <agustin_enriquez@hotmail.com>
2020-06-29 19:11:07 -0300

    createuser & loginmixin

--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from shop.views import ItemList, ItemDetailView, CartItemCreateView
+from shop.views import ItemList, ItemDetailView, CartItemCreateView, CreateUser, CartItemList
 
 
 urlpatterns = [
-        path("", ItemList.as_view(), name="main-shop"),
-        path("item/<int:pk>/", ItemDetailView.as_view(), name="item-detail"),
-        path("cartitem/add/<int:pk>", CartItemCreateView.as_view(), name="cartitem-create"),
+    path("", ItemList.as_view(), name="main-shop"),
+    path("item/<int:pk>/", ItemDetailView.as_view(), name="item-detail"),
+    path("cartitem/add/<int:pk>", CartItemCreateView.as_view(), name="cartitem-create"),
+    path("cartitem/", CartItemList.as_view(), name="cartitem-list"),
+    path("users/add", CreateUser.as_view(), name="add-user"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,9 @@
-from .models import Item, CartItem
+from .models import Item, CartItem, Cart, Profile
 from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from .forms import UserForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
@@ -14,3 +18,42 @@ class ItemDetailView(DetailView):
 class CartItemCreateView(CreateView):
     model = CartItem
     fields = "__all__"
+
+
+class CartItemList(LoginRequiredMixin, ListView):
+    model = CartItem
+    login_url = "/users/add"
+
+    def get_queryset(self):
+        return super().get_queryset().filter(cart__user=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.object_list.exists():
+            cart_total = sum([item.item.price for item in self.object_list])
+            self.object_list[0].cart.total = cart_total
+            self.object_list[0].cart.save()
+            context["cartitem_total"] = cart_total
+            context["cart_id"] = self.object_list[0].cart.id
+        else:
+            context["cartitem_total"] = 0
+        return context
+
+
+class CreateUser(CreateView):
+    model = User
+    form_class = UserForm
+    success_url = "/"
+    template_name = "web/create_user.html"
+
+    def form_valid(self, form):
+        """If the form is valid, save the associated model."""
+        self.object = form.save()
+        # Create profile with recently created User instance.
+        Profile.objects.create(user=self.object)
+        Cart.objects.create(user=self.object)
+        login(self.request, self.object)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return self.success_url
