b072e5d70e988505f38c2d7a5f5a1089a6daf3ef
m0tz <agustin_enriquez@hotmail.com>
2020-07-05 19:16:38 -0300

    created_by field

--- shop/admin.py
+++ shop/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from .models import Item
+
 # Register your models here.
 
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "description", "created_by")
 
-    class Meta:
-        model = Item
-        fields = '__all__'
+    def save_model(self, request, obj, form, change):
+        obj.created_by = request.user
+        return super().save_model(request, obj, form, change)
--- shop/models.py
+++ shop/models.py
@@ -8,6 +8,7 @@ class Item(models.Model):
     name = models.CharField(max_length=50, unique=False)
     price = models.DecimalField(max_digits=4, decimal_places=2)
     description = models.TextField(max_length=300, default=None, null=True)
+    created_by = models.ForeignKey("auth.User", on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.name
